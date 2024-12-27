373600d43f802c762b286253a8bc224c48f882bb
Luan Saboia <luan.saboia@gmail.com>
2024-12-27 16:08:03 -0300

    contact complete with send email

--- script.js
+++ script.js
@@ -12,3 +12,30 @@ toggleButtons.forEach(button => {
         }
     });
 });
+
+const form = document.querySelector('.contact-form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault(); // Evita o envio real do formulário
+
+    alert('Obrigado por entrar em contato! Responderei em breve.');
+    form.reset(); // Limpa os campos do formulário
+});
+
+// Inicialização do EmailJS com a Public Key
+emailjs.init("duZfPJZEZvIGRc38z"); // Substitua pela sua Public Key
+
+// Evento de envio do formulário
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Previne o comportamento padrão do formulário
+
+    // Envio do formulário usando `sendForm`
+    emailjs.sendForm("service_fs3hsgq", "template_skz77dj", this)
+        .then(() => {
+            alert("Mensagem enviada com sucesso!");
+            document.getElementById("contact-form").reset(); // Limpa os campos
+        }, (error) => {
+            alert("Erro ao enviar a mensagem. Tente novamente.");
+            console.error("Erro:", error);
+        });
+});
